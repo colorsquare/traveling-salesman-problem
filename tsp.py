bd647e871277e2f2dce97c8e252b455e25036695
Hyoung Jo, Bhang <hyoungjo.bhang@gmail.com>
2022-08-03 13:09:21 +0900

    feat(TSP): create class TSP

---
+++ tsp.py
@@ -0,0 +1,16 @@
+class TSP:
+    """An instance of traveling salesman problem.
+
+    Note:
+        Number of cities in an instance is `n`.
+        Cities does not have names. Instead, they are referred by indexes in a list.
+
+    Attributes:
+        locations (list[tuple[int]]): Each city location is represented as tuple[int], an euclidien coordinate (x, y).
+        distances (list[list[int]]): Pre-calculated distances between all cities. n x n matrix.
+        representation (list[int]): List of cities, in order of salesman's travel.
+    """
+
+    def __init__(self):
+        self.coordinates = []
+
