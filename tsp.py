ffe18d0bcf2b40339b2c91a4c9d1dfe495bba738
Hyoung Jo, Bhang <hyoungjo.bhang@gmail.com>
2022-08-06 18:00:58 +0900

    misc(TSP): update docstring grammar

--- tsp.py
+++ tsp.py
@@ -1,12 +1,13 @@
 class TSP:
-    """An instance of traveling salesman problem.
+    """An instance of traveling salesman problem (TSP).
 
     Note:
         Number of cities in an instance is `n`.
         Cities do not have names. Instead, they are referred by indexes in a list.
 
     Attributes:
-        locations (list[tuple[int]]): Each city location is represented as tuple[int], an euclidien coordinate (x, y).
+        locations (list[tuple[int]]): Each city location is represented as a tuple[int],
+            an euclidien coordinate (x, y).
         distances (list[list[int]]): Pre-calculated distances between all cities. n x n matrix.
         representation (list[int]): List of cities, in order of salesman's travel.
     """
