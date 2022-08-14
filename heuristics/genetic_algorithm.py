cde5010fff0048d9788197b5441c070d98e87912
Hyoung Jo, Bhang <hyoungjo.bhang@gmail.com>
2022-08-15 00:24:47 +0900

    refactor(ga): modularize insertion_sort

--- heuristics/genetic_algorithm.py
+++ heuristics/genetic_algorithm.py
@@ -5,6 +5,27 @@ Heuristic approach imitating biological evolution in nature.
 """
 
 
+def insertion_sort(generation, elem):
+    """Insert a new element to a generation.
+
+    Args:
+        generation (list[tuple]): List of elements `elem` below.
+            Sorted in order of distance, elem[1].
+        elem (tuple[list[int], int]): A tuple of route and its distance of class TSP.
+
+    Returns:
+        generation (list[tuple]): List with `elem` inserted.
+    """
+    route, total_distance = elem
+
+    insert_pos = 0
+    while insert_pos < len(generation) and total_distance > generation[insert_pos][1]:
+        insert_pos += 1
+    generation.insert(insert_pos, (route, total_distance))
+
+    return generation
+
+
 def genetic_algorithm(tsp):
     """Traveling salesman problem (TSP) solved with genetic algorithm.
 
@@ -22,14 +43,7 @@ def genetic_algorithm(tsp):
         for _ in range(population):
             new_route = tsp.create_random_route()
             new_total_distance = tsp.calculate_total_distance(new_route)
-
-            insert_pos = 0
-            while (
-                insert_pos < len(first_generation)
-                and new_total_distance > first_generation[insert_pos][1]
-            ):
-                insert_pos += 1
-            first_generation.insert(insert_pos, (new_route, new_total_distance))
+            insertion_sort(first_generation, (new_route, new_total_distance))
 
         return first_generation
 
