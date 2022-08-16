5525b4a44affe7458ce5d1e9582412853a7e613d
Hyoung Jo, Bhang <hyoungjo.bhang@gmail.com>
2022-08-16 10:47:01 +0900

    misc(ga): remove argument 'population'

--- heuristics/genetic_algorithm.py
+++ heuristics/genetic_algorithm.py
@@ -37,9 +37,10 @@ def genetic_algorithm(tsp):
         tsp.total_distance (int): Total distance calculated with tsp.route.
     """
 
-    def create_first_generation(population):
+    def create_first_generation():
         first_generation = []
 
+        population = len(tsp.distances)
         for _ in range(population):
             new_route = tsp.create_random_route()
             new_total_distance = tsp.calculate_total_distance(new_route)
@@ -48,16 +49,9 @@ def genetic_algorithm(tsp):
         return first_generation
 
     def create_child_generation(parent_generation):
-        # TODO
-        population = len(parent_generation)
-        n = population // 2
-        return sorted(
-            parent_generation[:n] + create_first_generation(population - n),
-            key=lambda e: e[1],
-        )
+        return parent_generation
 
-    population = len(tsp.distances)
-    parent_generation = create_first_generation(population)
+    parent_generation = create_first_generation()
 
     no_evolution_count = 0
     while no_evolution_count < 3:
